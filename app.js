8e16e6d1cf53c363e1ca8c70b5e292a16d5c3be3
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-10-24 18:04:16 -0400

    changed files from jade to pug in order to fix inline problem, trying to do a favicon

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require("body-parser")
 
 var index = require("./routes/index")
 var users = require("./routes/users")
+var favicon = require("serve-favicon")
 
 var app = express()
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "jade")
+app.set("view engine", "pug")
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +22,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")))
 
 app.use("/", index)
 app.use("/users", users)
